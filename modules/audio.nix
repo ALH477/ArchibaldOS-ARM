d270f478a8fceebf5c24930d18d7f7a3b385c66a
Asher <146795551+ALH477@users.noreply.github.com>
2025-11-13 04:42:08 -0800

    Update audio.nix

--- modules/audio.nix
+++ modules/audio.nix
@@ -15,7 +15,7 @@ in {
   musnix.rtirq.enable = true;
   musnix.das_watchdog.enable = true;
 
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;  # Updated from hardware.pulseaudio
 
   services.pipewire = {
     enable = true;
